c33904f4f8a797935dd4b59398b90a6a2fe6222b
Alexander Fernando León Díaz <ing.afleon@gmail.com>
2025-08-21 09:32:16 -0500

    Basic project creation

--- Fantasy.Frontend/Program.cs
+++ Fantasy.Frontend/Program.cs
@@ -1,11 +1,11 @@
+using Fantasy.Frontend;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Fantasy.Frontend;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7255") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
